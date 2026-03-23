19ac6d64de7fbbc8f0db04e41e835e08aea7d737
Skronky <goyabean64@gmail.com>
2026-03-23 04:19:39 -0400

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,4 +1,4 @@
-const ytdl = require('ytdl-core');
+const play = require('play-dl');
 
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -7,10 +7,12 @@ module.exports = async function handler(req, res) {
   if (!id) return res.status(400).json({ error: 'Missing video id' });
 
   try {
-    const info = await ytdl.getInfo('https://www.youtube.com/watch?v=' + id);
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'audioonly' });
-    if (!format) return res.status(404).json({ error: 'No audio format found' });
-    res.json({ url: format.url });
+    const info = await play.video_info('https://www.youtube.com/watch?v=' + id);
+    const formats = info.format.filter(f => f.mimeType && f.mimeType.includes('audio'));
+    formats.sort((a, b) => (b.bitrate || 0) - (a.bitrate || 0));
+    const best = formats[0];
+    if (!best) return res.status(404).json({ error: 'No audio format found' });
+    res.json({ url: best.url });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }

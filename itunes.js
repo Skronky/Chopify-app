3cbf8a08ef51f6c1652cb1bfa5892b34cf6c4e2f
Skronky <goyabean64@gmail.com>
2026-03-23 02:02:21 -0400

    Delete itunes.js

--- itunes.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const { q } = req.query;
-  const r = await fetch(`https://itunes.apple.com/search?term=${q}&entity=song&limit=5&country=us`);
-  const data = await r.json();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json(data);
-}
